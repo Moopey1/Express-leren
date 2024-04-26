e8a8899742825dee324d1a3494d4a824a3585674
Moopey1 <142419639+Moopey1@users.noreply.github.com>
2024-04-26 14:42:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 const express = require('express');
 const path = require('path');
+const { title } = require('process');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'pug');
+
+// stringifies object, found on the internet
+// https://stackoverflow.com/questions/11616630/how-can-i-print-a-circular-structure-in-a-json-like-format/11616993#11616993
+JSON.safeStringify = (obj, indent = 2) => {
+    let cache = [];
+    const retVal = JSON.stringify(
+        obj,
+        (key, value) =>
+        typeof value === "object" && value !== null ?
+        cache.includes(value) ?
+        undefined // Duplicate reference found, discard key
+        :
+        cache.push(value) && value // Store value in our collection
+        :
+        value,
+        indent
+    );
+    cache = null;
+    return retVal;
+};
+
+// sets absolute path to public directory for static file use
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// get http request for homepage, redirects to index.html
 app.get('/', (req, res) => {
-    res.redirect('/public/index.html');
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
 });
 
+// app.get('/', (req, res) => {
+//     const request = JSON.safeStringify(req);
+//     console.log(`Request in for homepage: ${request}`);
+//     res.redirect('/public/index.html');
+// });
+
+// get http request for /users page
 app.get('/users', (req, res) => {
-    res.send(`You're looking for users!`);
+    const request = JSON.safeStringify(req);
+    console.log(`Request in for user: ${request}`);
+    res.redirect('/public/users.html');
 });
 
+// get http request for /download
 app.get("/download", (req, res) => {
+    const request = JSON.safeStringify(req);
+    console.log(`Request in for download: ${request}`);
     res.download(path.join(__dirname, "/test.txt"), "test.txt", err => {
         if (err) console.log(err);
     });
 });
 
+// starts the server, listens for port 3000 and logs when ready 
 app.listen(port, () => {
     console.log(`My learning app is running on ${port}`);
 });
